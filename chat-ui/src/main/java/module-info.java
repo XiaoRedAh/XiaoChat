0794948fcd062b98d20a5aba993deb705025a72f
XiaoRedAh <2976698583@qq.com>
2023-12-01 18:06:37 +0800

    项目统一采用Oracle JDK8

--- chat-ui/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.itstack.naive.chat.ui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-    opens org.chat.ui to javafx.fxml;
-    exports org.chat.ui;
-}
